4043da08da347cdf63e4e90eaf593e7278a80d71
Beatriz Maia <149596621+beatrizmaiar@users.noreply.github.com>
2024-05-08 14:51:14 -0300

    Delete lista 1 directory

--- lista 1/conceitosiniciais8.py
+++
@@ -1,8 +0,0 @@
-"""Crie um programa que calcule o tempo de viagem, pedindo ao usuário a distância e a velocidade."""
-
-distancia = float(input('digite a distancia da viagem que será realizada: '))
-velocidade = float(input('digite a velocidade em que você dirigirá: '))
-
-tempo = distancia/velocidade
-
-print(f'o tempo da viagem é de {tempo} horas')
